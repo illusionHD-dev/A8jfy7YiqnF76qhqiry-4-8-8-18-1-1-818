1966707dbef96287591ccab2b83c0156bf54d48a
illusionHD-dev <joeyetchison@gmail.com>
2026-09-04 23:16:58 -0500

    Add files via upload

--- main.lua
+++ main.lua
@@ -1,11 +1,20 @@
+-- Local customization: dismiss the full-screen loader after initialization.
 repeat task.wait() until game:IsLoaded()
+local launchEnvironment = (getgenv and getgenv()) or _G
+local riseRequested = launchEnvironment.Rise
+if riseRequested == nil then riseRequested = shared.Rise end
+shared.Rise = riseRequested == true
 if shared.vape then shared.vape:Uninject() end
 
 local vape
 local loadstring = function(...)
 	local res, err = loadstring(...)
-	if err and vape then
-		vape:CreateNotification('Vape', 'Failed to load : '..err, 30, 'alert')
+	if err then
+		warn('Vape failed to compile: '..tostring(err))
+		if vape then
+			vape:CreateNotification('Vape', 'Failed to load : '..err, 30, 'alert')
+		end
+		error(err, 2)
 	end
 	return res
 end
@@ -20,11 +29,12 @@ local cloneref = cloneref or function(obj)
 	return obj
 end
 local playersService = cloneref(game:GetService('Players'))
+local loading = loadstring(readfile('newvape/guis/loading.lua'), 'Vape loading screen')()
 
 local function downloadFile(path, func)
 	if not isfile(path) then
 		local suc, res = pcall(function()
-			return game:HttpGet('https://raw.githubusercontent.com/illusionHD-dev/A8jfy7YiqnF76qhqiry-4-8-8-18-1-1-818/'..readfile('newvape/profiles/commit.txt')..'/'..select(1, path:gsub('newvape/', '')), true)
+			return game:HttpGet('https://raw.githubusercontent.com/7GrandDadPGN/illusionhd-dev/A8jfy7YiqnF76qhqiry-4-8-8-18-1-1-818/'..readfile('newvape/profiles/commit.txt')..'/'..select(1, path:gsub('newvape/', '')), true)
 		end)
 		if not suc or res == '404: Not Found' then
 			error(res)
@@ -39,7 +49,11 @@ end
 
 local function finishLoading()
 	vape.Init = nil
+	loading:WaitForMinimumDisplay()
 	vape:Load()
+	if vape.HideLoadingScreen then
+		vape:HideLoadingScreen()
+	end
 	task.spawn(function()
 		repeat
 			vape:Save()
@@ -56,12 +70,13 @@ local function finishLoading()
 				if shared.VapeDeveloper then
 					loadstring(readfile('newvape/loader.lua'), 'loader')()
 				else
-					loadstring(game:HttpGet('https://raw.githubusercontent.com/illusionHD-dev/A8jfy7YiqnF76qhqiry-4-8-8-18-1-1-818/'..readfile('newvape/profiles/commit.txt')..'/loader.lua', true), 'loader')()
+					loadstring(game:HttpGet('https://raw.githubusercontent.com/7GrandDadPGN/illusionhd-dev/A8jfy7YiqnF76qhqiry-4-8-8-18-1-1-818/'..readfile('newvape/profiles/commit.txt')..'/loader.lua', true), 'loader')()
 				end
 			]]
 			if shared.VapeDeveloper then
 				teleportScript = 'shared.VapeDeveloper = true\n'..teleportScript
 			end
+			teleportScript = 'shared.Rise = '..tostring(shared.Rise == true)..'\n'..teleportScript
 			if shared.VapeCustomProfile then
 				teleportScript = 'shared.VapeCustomProfile = "'..shared.VapeCustomProfile..'"\n'..teleportScript
 			end
@@ -81,13 +96,28 @@ end
 if not isfile('newvape/profiles/gui.txt') then
 	writefile('newvape/profiles/gui.txt', 'new')
 end
-local gui = 'new'--readfile('newvape/profiles/gui.txt')
+local gui = shared.Rise and 'rise-v7' or 'new'
+if shared.Rise then
+	assert(isfile('newvape/guis/rise-v7.lua'), 'Rise v7 is missing: install newvape/guis/rise-v7.lua')
+end
 
 if not isfolder('newvape/assets/'..gui) then
 	makefolder('newvape/assets/'..gui)
 end
 vape = loadstring(downloadFile('newvape/guis/'..gui..'.lua'), 'gui')()
 shared.vape = vape
+vape.HideLoadingScreen = function(_, immediate)
+	loading:HideLoadingScreen(immediate)
+end
+vape:Clean(function()
+	if loading.LoadingScreen then
+		loading.LoadingScreen:Destroy()
+		loading.LoadingScreen = nil
+	end
+end)
+if loading.LoadingStatus then
+	loading.LoadingStatus.Text = 'Loading Vape\nPreparing game modules'
+end
 
 if not shared.VapeIndependent then
 	loadstring(downloadFile('newvape/games/universal.lua'), 'universal')()
@@ -96,7 +126,7 @@ if not shared.VapeIndependent then
 	else
 		if not shared.VapeDeveloper then
 			local suc, res = pcall(function()
-				return game:HttpGet('https://raw.githubusercontent.com/illusionHD-dev/A8jfy7YiqnF76qhqiry-4-8-8-18-1-1-818/'..readfile('newvape/profiles/commit.txt')..'/games/'..game.PlaceId..'.lua', true)
+				return game:HttpGet('https://raw.githubusercontent.com/7GrandDadPGN/illusionhd-dev/A8jfy7YiqnF76qhqiry-4-8-8-18-1-1-818/'..readfile('newvape/profiles/commit.txt')..'/games/'..game.PlaceId..'.lua', true)
 			end)
 			if suc and res ~= '404: Not Found' then
 				loadstring(downloadFile('newvape/games/'..game.PlaceId..'.lua'), tostring(game.PlaceId))(...)
